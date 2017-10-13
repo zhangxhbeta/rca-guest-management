32f2c7f12d4982d1ece3a9800e8f5ab9bd27158a
zhangxhbeta <zhangxhbeta@gmail.com>
2017-10-13 10:38:16 +0800

    配置运行测试

--- src/containers/MockApiRequest/MockApiRequest.test.js
+++ src/containers/MockApiRequest/MockApiRequest.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
-import { App } from './';
+import MAR from './';
 
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 // noinspection JSUnresolvedFunction
 it('renders without crashing', () => {
-  shallow(<App />);
+  shallow(<MAR />);
 });
